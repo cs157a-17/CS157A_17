80d7027f1b6972902dc2783a26fd5ee58c32ce26
Ge Ou <ougejiajia@gmail.com>
2019-09-26 11:42:01 -0700

    nodejs express database connect

---
+++ connectTest/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+module.exports = router;
